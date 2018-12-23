693fd9568774bdd8f6e47f785adda25db45cd76e
ashleybeshir <ashleybeshir0@gmail.com>
2018-12-23 19:22:48 +0000

    testing out to see if it will compile and send error message

---
+++ gatsby_compiler.js
@@ -0,0 +1,30 @@
+const { spawn } = require('child_process');
+const axios = require('axios');
+
+if(process.argv[2] === undefined || process.argv[3])
+{
+    throw Error ('Argument missing');
+}
+const ls = spawn('sh', ['./gatsby_script.sh', process.argv[2]]);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    axios.post(process.argv[3], {
+            error: 'data',
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+    });
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
